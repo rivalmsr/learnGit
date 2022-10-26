bd59c1833ae5f3fc0c655905ab8440f7779f6d9e
Rival Msr <rivalmusripal@gmail.com>
2022-10-27 06:24:47 +0700

    add account function

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import Main from "./main"
 import SecondMain from "./second-main"
+import Account from "./account"
+
 function Index(){
   return(
     <>
       <Main />
       <SecondMain />
+      <Account />
     </>
   ) 
 }
